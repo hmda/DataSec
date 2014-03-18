f5a99cbac446db1d6a12982928942bced5427965
ducanh <haminhducanh@gmail.com>
2014-03-18 16:19:35 +0700

    Post lại

---
+++ src/io/InOu.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package io;
+
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import vn.hus.nlp.tokenizer.VietTokenizer;
+import jvntagger.POSTagger;
+import jvntagger.MaxentTagger;
+
+/**
+ *
+ * @author N56V
+ */
+public class InOu {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static String[] words = new String[31];
+    private static long[] amount = new long[31];
+        
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Filter filter = new Filter();
+        Tagger tagger = new Tagger();
+        Changer changer = new Changer();
+        VietTokenizer tokenizer = new VietTokenizer();
+        filter.readWriteFile("baomoi.tsv","baomoi.content.20140315.txt");   // Lọc nội dung trong thẻ "content"
+        tokenizer.tokenize("baomoi.content.20140315.txt", "token.txt"); // Xác định các từ.
+        tagger.tag("token.txt", "tag.txt"); // Phân loại các từ
+        changer.change("tag.txt", "AnhHMD_20140317_NOUN.tsv");  // Chuyển đổi các từ theo yêu cầu, in ra file kết quả cuối cùng.
+    }
+}
+
